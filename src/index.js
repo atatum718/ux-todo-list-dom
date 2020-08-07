328d72d25fca55e88c9d9c6dfb4272d39c7c4f34
atatum718 <54254894+atatum718@users.noreply.github.com>
2020-08-07 16:31:27 -0400

    added HTML elements using javascript

--- src/index.js
+++ src/index.js
@@ -1,32 +1,62 @@
-
 let todos = [
-    {
-        completed: false,
-        description: "Take Jim to the hair salon"
-    },
-    {
-        completed: true,
-        description: "Drop off wedding invitation at mailbox"
-    },
-    {
-        completed: false,
-        description: "Pick up the cake"
-    },
-    {
-        completed: false,
-        description: "Call the caterers"
-    }
+  {
+    completed: false,
+    description: "Take Jim to the hair salon",
+  },
+  {
+    completed: true,
+    description: "Drop off wedding invitation at mailbox",
+  },
+  {
+    completed: false,
+    description: "Pick up the cake",
+  },
+  {
+    completed: false,
+    description: "Call the caterers",
+  },
 ];
 
 function renderTodoApp() {
-    let app = document.querySelector("#app")
-    let h1 = document.createElement("h1")
-    // your code here
+  let app = document.querySelector("#app");
+  let h1 = document.createElement("h1");
+  // your code here
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  let twoListItems = document.createElement("ul");
+  let list = `
+    <ul>
+      <li class = "todo-item">
+          <input type= "checkbox"/>
+          Take Jim to the hair salon
+      </li>
+      <li class - "todo-item"
+          <input type = "checkbox"/>
+          Drop off wedding invitatation at mailbox
+      </li>
+  </ul>`;
+  document.body.append(twoListItems);
+  for (let i = 0; i < todos.length; i++) {
+    let twoListItems = todos[i];
+    let checkBoxesList = ` 
+    <li class = 'todo-item'>
+        <p> ${twoListItems.completed}<p>
+        <p> ${twoListItems.description}<p>
+    </li>`;
+  }
+  twoListItems.innerHTML += list;
 
-    h1.innerText = "Todo List"
+  h1.innerText = "Todo List";
 
-    app.appendChild(h1)
-    // and maybe some here
+  app.appendChild(h1);
+  // and maybe some here
+
+  //   twoListItems.innerText =
+  //     "Take Jim to the hair salon" + "Drop off wedding invitation at mailbox";
+  //   twoListItems.appendChild(checkbox, list);
+  //   let newText = document.getElementById("app");
+  //   document.body.append(newText);
+  //   twoListItems.innertext += checkBoxesList;
 }
 
-renderTodoApp()
+renderTodoApp();
---
+++ src/index2.js
@@ -0,0 +1,41 @@
+let todos = [
+  {
+    completed: false,
+    description: "Take Jim to the hair salon",
+  },
+  {
+    completed: true,
+    description: "Drop off wedding invitation at mailbox",
+  },
+  {
+    completed: false,
+    description: "Pick up the cake",
+  },
+  {
+    completed: false,
+    description: "Call the caterers",
+  },
+];
+
+function renderTodoApp() {
+  let app = document.querySelector("#app");
+  let h1 = document.createElement("h1");
+  // your code here
+  let todoList = document.createElement("ul");
+
+  h1.innerText = "Todo List";
+  app.appendChild(h1);
+  app.appendChild(todoList);
+
+  for (let i = 0; i < todos.length; i++) {
+    let todoItems = todos[i];
+    let stateOfCheckBox = todoItems.completed ? "checked" : "";
+    let todoMarkup = `
+    <li><input type="checkbox" ${stateOfCheckBox}/>${todoItems.description}</li>
+    `;
+    todoList.innerHTML += todoMarkup;
+  }
+  // and maybe some here
+}
+
+renderTodoApp();
